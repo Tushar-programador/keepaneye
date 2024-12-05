5cd0ccb279532a503e958a37d698665d73712ce5
tushar-programador <tusharkalra307@gmail.com>
2024-12-05 23:24:49 +0530

    try to add ejs

--- main/src/app.js
+++ main/src/app.js
@@ -13,6 +13,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", "./views"); // Ensure all EJS files are inside the 'views' folder
+app.use(express.static("public"));
+
 
 import router from "./routes/index.js";
 
